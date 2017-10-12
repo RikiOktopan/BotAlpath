24aa0ea5a8de11d8ba0ac1eebdf1e08b388a2615
RikiOktopan <rikioktopan1@gmail.com>
2017-10-13 00:51:37 +0700

    Rename main.js to main1.js

---
+++ src/main1.js
@@ -0,0 +1,121 @@
+const LineAPI = require('./api');
+const { Message, OpType, Location } = require('../curve-thrift/line_types');
+let exec = require('child_process').exec;
+
+const myBot = ['u0f020150405fe23cace51efbbe590a76'];
+
+
+function isAdminOrBot(param) {
+    return myBot.includes(param);
+}
+
+
+class LINE extends LineAPI {
+    constructor() {
+        super();
+        this.receiverID = '';
+        this.checkReader = [];
+        this.stateStatus = {
+            cancel: 0,
+            kick: 0,
+        }
+    }
+
+    getOprationType(operations) {
+        for (let key in OpType) {
+            if(operations.type == OpType[key]) {
+                if(key !== 'NOTIFIED_UPDATE_PROFILE') {
+                    console.info(`[* ${operations.type} ] ${key} `);
+                }
+            }
+        }
+    }
+
+    poll(operation) {
+        if(operation.type == 25 || operation.type == 26) {
+            // console.log(operation);
+            const txt = (operation.message.text !== '' && operation.message.text != null ) ? operation.message.text : '' ;
+            let message = new Message(operation.message);
+            this.receiverID = message.to = (operation.message.to === myBot[0]) ? operation.message.from_ : operation.message.to ;
+            Object.assign(message,{ ct: operation.createdTime.toString() });
+            this.textMessage(txt,message)
+        }
+
+        if(operation.type == 13 && this.stateStatus.cancel == 1) {
+            this.cancelAll(operation.param1);
+        }
+
+        if(operation.type == 19) { //ada kick
+            // op1 = group nya
+            // op2 = yang 'nge' kick
+            // op3 = yang 'di' kick
+            if(isAdminOrBot(operation.param3)) {
+                this._invite(operation.param1,[operation.param3]);
+            }
+            if(!isAdminOrBot(operation.param2)){
+                this._kickMember(operation.param1,[operation.param2]);
+            } 
+
+        }
+
+        if(operation.type == 55){ //ada reader
+
+            const idx = this.checkReader.findIndex((v) => {
+                if(v.group == operation.param1) {
+                    return v
+                }
+            })
+            if(this.checkReader.length < 1 || idx == -1) {
+                this.checkReader.push({ group: operation.param1, users: [operation.param2], timeSeen: [operation.param3] });
+            } else {
+                for (var i = 0; i < this.checkReader.length; i++) {
+                    if(this.checkReader[i].group == operation.param1) {
+                        if(!this.checkReader[i].users.includes(operation.param2)) {
+                            this.checkReader[i].users.push(operation.param2);
+                            this.checkReader[i].timeSeen.push(operation.param3);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(operation.type == 13) { // diinvite
+            if(isAdminOrBot(operation.param2)) {
+                return this._acceptGroupInvitation(operation.param1);
+            } else {
+                return this._cancel(operation.param1,myBot);
+            }
+        }
+        this.getOprationType(operation);
+    }
+
+    async cancelAll(gid) {
+        let { listPendingInvite } = await this.searchGroup(gid);
+        if(listPendingInvite.length > 0){
+            this._cancel(gid,listPendingInvite);
+        }
+    }
+
+    async searchGroup(gid) {
+        let listPendingInvite = [];
+        let thisgroup = await this._getGroups([gid]);
+        if(thisgroup[0].invitee !== null) {
+            listPendingInvite = thisgroup[0].invitee.map((key) => {
+                return key.mid;
+            });
+        }
+        let listMember = thisgroup[0].members.map((key) => {
+            return { mid: key.mid, dn: key.displayName };
+        });
+
+        return { 
+            listMember,
+            listPendingInvite
+        }
+    }
+
+    setState(seq) {
+        if(isAdminOrBot(seq.from)){
+            let [ actions , status ] = seq.text.split(' ');
+            const action = actions.toLowerCase();
+       
